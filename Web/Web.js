0d1a7571668a4ef76c5c465dff320402a071c809
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-07-13 11:46:26 -0700

    webfiles

--- Web/Web.js
+++ Web/Web.js
@@ -1,19 +1,38 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to Web.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+Players = new Meteor.Collection("players");
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.Router.add('/update/:playerId/:x/:y/:rotationZ','GET',function(playerId,x,y,rotationZ) {
+    var id = 0;
+    try {
+      if (Players.find({playerId:playerId}).count() > 0) {
+        Players.update({playerId:playerId},{$set:{x:x,y:y,rotationZ:rotationZ}});
+      } else {
+        id = Players.insert({playerId:playerId,x:x,y:y,rotationZ:rotationZ});
+      }
+    } catch (e) {
+      return [500,'0'];
+    }
+    return [200,Players.find({}).count().toString()];
+  });
+
+  Meteor.Router.add('/list','GET',function() {
+    return [200,JSON.stringify({success:true,data:Players.find({}).fetch()})];
+  });
+
+  Meteor.Router.add('/clear','GET',function() {
+    Players.remove({});
+    return [200,'1'];
   });
 }
+
+if (Meteor.isClient) {
+  Template.radarTemplate.players = function () {
+    return Players.find();
+  }
+}
+
+Meteor.methods({
+  updateHandler:function(playerId,x,y,rotationZ) {
+
+  }
+})
