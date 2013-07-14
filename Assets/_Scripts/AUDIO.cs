66f50f4d6ccd9d5b0ee0545ddaba6eb9fff96773
Sergio Torcanin <sergio.torcanin@scopely.com>
2013-07-14 12:43:20 -0700

    FGHJKL

--- Assets/_Scripts/AUDIO.cs
+++ Assets/_Scripts/AUDIO.cs
@@ -7,4 +7,12 @@ public class AUDIO : MonoBehaviour {
 	public AudioClip explosionSound;
 	public AudioClip collectSound;
 	public AudioClip shootSound;
+
+	public AudioClip[] killSound;
+
+	public void PlayRandomKillSound()
+	{
+		int rand = Random.Range(0, killSound.Length-1);
+		AudioSource.PlayClipAtPoint(killSound[rand], transform.position);
+	}
 }
