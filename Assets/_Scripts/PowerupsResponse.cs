0f153a62ae7c850630d6b51f435aee7f1d6f2244
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-07-13 19:43:26 -0700

    networking 13

---
+++ Assets/_Scripts/PowerupsResponse.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public class PowerupsResponse
+{
+    public bool success;
+    public List<PowerupResponseDto> data;
+    public PowerupsResponse () {}
+}
+
+
--- Web/Web.js
+++ Web/Web.js
@@ -1,4 +1,5 @@
 Players = new Meteor.Collection("players");
+Powerups = new Meteor.Collection("powerups");
 
 defaultPreserve = {
   'div[id]': function (node) {
@@ -16,7 +17,8 @@ if (Meteor.isServer) {
             {$set: {x: x, y: y, rotationZ: rotationZ, color: color}});
         } else {
           id =
-            Players.insert({playerId: playerId, x: x, y: y, rotationZ: rotationZ, color: color});
+            Players.insert({playerId: playerId, x: x, y: y,
+              rotationZ: rotationZ, color: color});
         }
       } catch (e) {
         return [500, '0'];
@@ -24,9 +26,10 @@ if (Meteor.isServer) {
       return [200, Players.find({}).count().toString()];
     });
 
-  Meteor.Router.add('/list', 'GET', function () {
-    return [200, JSON.stringify({success: true, data: Players.find({}).fetch()})
-    ];
+  Meteor.Router.add('/powerups', 'GET', function () {
+    var data = Powerups.find({}).fetch();
+    Powerups.remove({});
+    return [200, JSON.stringify({success: true, data: data})];
   });
 
   Meteor.Router.add('/clear', 'GET', function () {
@@ -40,19 +43,27 @@ if (Meteor.isServer) {
 }
 
 var scale = 10;
-var origin = [160,240];
+var origin = [160, 240];
 
 if (Meteor.isClient) {
+
+  Template.radarTemplate.events = {
+    'tap, click #container': function (e) {
+      Powerups.insert({x: (e.pageX - origin[0]) / scale,
+        y: (e.pageY - origin[1]) / -scale, type: 2});
+    }
+  };
+
   Template.radarTemplate.players = function () {
-    return _.map(Players.find({}).fetch(),function(p) {
-      p.y*=-1;
-      p.y*=scale;
-      p.x*=scale;
-      p.x+=origin[0];
-      p.y+=origin[0];
+    return _.map(Players.find({}).fetch(), function (p) {
+      p.y *= - 1;
+      p.y *= scale;
+      p.x *= scale;
+      p.x += origin[0];
+      p.y += origin[0];
       return p;
     });
-  }
+  };
 
   Template.radarTemplate.preserve = defaultPreserve;
 }
